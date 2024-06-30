3a1cc05aa69d2944171e514178a5b73e4e775ec0
harini.bathula <harini.bathula@techigai.io>
2024-06-30 22:05:26 +0530

    ISP

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Interface_Segregation_Principle.Bad_Examples.Implementations.Example9_Vehicle_Type;
+using Interface_Segregation_Principle.Bad_Examples.Interfaces.Example9_Vehicle_Type;
+using Interface_Segregation_Principle.Good_Examples.Interfaces.Example9_Vehicle_Type;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Bad example usage (should illustrate why it's bad)
+        IVehicle badCar = new Car();
+        badCar.Drive();
+        // badCar.Fly(); // This would throw an exception
+
+        IVehicle badAirplane = new Aeroplane();
+        // badAirplane.Drive(); // This would throw an exception
+        badAirplane.Fly();
+
+        // In a real application, avoid using the bad example to prevent ISP violation issues
+    }
+}
